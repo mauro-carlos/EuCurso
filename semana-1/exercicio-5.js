de08ea3090d86d64f481064bb459ab96e982d4f8
Mauro Carlos <mauro_carlos14@hotmail.com>
2022-11-16 08:26:15 -0400

    exercicio-5 da semana-1 eucurso

---
+++ semana-1/exercicio-5.js
@@ -0,0 +1,5 @@
+let produto = "moveis";
+let precoProduto = 100;
+let percentual = 10;
+let precoTotal = precoProduto - (precoProduto * percentual / 100);
+console.log(` o preço do produto que era ${precoProduto} agora é com desconto de ${precoTotal}`);
