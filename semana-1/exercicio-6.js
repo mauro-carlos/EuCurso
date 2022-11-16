432b529fa48ca37ac33db719d5ed393182f9e7be
Mauro Carlos <mauro_carlos14@hotmail.com>
2022-11-16 11:38:42 -0400

    exercicio-6 da semana-1 eucurso

---
+++ semana-1/exercicio-6.js
@@ -0,0 +1,6 @@
+let salarioFixo = 2500;
+let vendas = 6000;
+let percentualVendas = (vendas * 4)/100;
+let salarioLiquido = salarioFixo + percentualVendas;
+
+console.log(`O salario que o funcionario ira receber é o salario liquido de ${salarioLiquido}`)
