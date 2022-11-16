b2a186f66215e6617959d29323082dff20086f1a
Mauro Carlos <mauro_carlos14@hotmail.com>
2022-11-16 08:17:08 -0400

    exercicio-4.js da semana-1 eucurso

---
+++ semana-1/exercicio-3.js
@@ -0,0 +1,8 @@
+let divisor = 20;
+let dividendo = 50;
+if (dividendo != 0){
+    resultado = divisor / dividendo;
+    console.log (`Resultado da divisao é ${resultado}`);
+}else {
+    console.log(`Nao é possivel dividir por ${dividendo}`);
+}
---
+++ semana-1/exercicio-4.js
@@ -0,0 +1,8 @@
+
+let nota1 = 5
+let nota2 = 10
+pesoNota1 = nota1 * 2;
+pesoNota2 = nota2 * 3;
+mediaNota = (pesoNota1 + pesoNota2)/2;
+console.log(`A média da nota é : ${mediaNota}`)
+
