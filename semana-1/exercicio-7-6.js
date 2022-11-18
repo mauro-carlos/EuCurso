ce00d28961bb4735fbcebd7e7497a11fb070bdc3
Mauro Carlos <mauro_carlos14@hotmail.com>
2022-11-18 08:11:14 -0400

    programa controle de fluxo

--- semana-1/exercicio-7-6.js
+++ semana-1/exercicio-7-6.js
@@ -1,8 +1,8 @@
-let pesoAtual = 90
+let pesoAtual = 79
 let engordou15 = pesoAtual * 15/100;
-engordou15 = engordou15 + pesoAtual;
+engordou15 += pesoAtual;
 let emagreceu20 = pesoAtual * 20/100;
-emagreceu20 = (emagreceu20 - pesoAtual)
+emagreceu20 -= pesoAtual
 const emagreceu = Math.abs(emagreceu20);
 
 console.log(`se engordou  15% seu peso é ${engordou15} `)
